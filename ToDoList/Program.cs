84bcc16a8582488bf1c0b870720bf4b9823cde78
MarkTobys <marktobys0@gmail.com>
2025-05-08 23:12:42 +1000

    - Added comments for newItem function - added more detail to several comments - began development on user selection UI

--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -7,6 +7,9 @@ namespace ToDoList
 {
     class Program
     {
+        /*
+         * UI which displays options, tasks and collects user input for viewing, adding, editing and deleting tasks
+         */
         static void Main(string[] args)
         {
             string input; // generic placeholder used to read in text for temporary processes
@@ -16,12 +19,11 @@ namespace ToDoList
             DateTime date; // optional due date for task
             // dictionary used to store tasks (keys) and descriptions
             Dictionary<string, Item> taskList = new Dictionary<string, Item>();
-
             Console.WriteLine("Welcome to your to-do list");
-
+            // check to see if there are any existing tasks, if not, prompt whether the user would like to create one
             if (taskList.Count == 0)
             {
-                Console.WriteLine("You currently have no current tasks, would you like to add a task?");
+                Console.Write("You currently have no current tasks, would you like to add a task? ");
                 input = Console.ReadLine();
                 bool exitApp = YesNo(input);
                 // exit the application if the user answers no
@@ -32,19 +34,37 @@ namespace ToDoList
                     Thread.Sleep(2000);
                     System.Environment.Exit(1);
                 }
-                // get task information
+                // get task information otherwise
                 Console.Write("Enter a name for your task: ");
                 taskName = Console.ReadLine();
                 Console.Write("Enter a description for your task: ");
                 Item newTask = newItem();
                 taskList.Add(taskName, newTask);
             }
-            string temp = taskList.Keys.First();
-            Console.WriteLine(temp);
-            Console.WriteLine($"{taskList[temp].Description}\n{taskList[temp].DueDate}");
-            
+            // ask the user whether they would like to create a new task or edit/delete an existing one
+            // this is created with a "simulated" selection screen which uses an index based cursor and refreshes with screen clears
+            Console.Clear();
+            int taskIndex = 0;
+            string[] options = {"Add", "Edit", "Delete"};
+            // print the initial selection screen
+            Console.WriteLine("Please select an option from the following");
+            for (int i = 0; i < 3; i++)
+            {
+                if (i == taskIndex)
+                {
+                    Console.Write(">");
+                }
+                Console.WriteLine(options[i]);
+            }           
+
         }
 
+        /* 
+         * function used to return a new Item class which stores a tasks description and due date (if given)
+         * The user is prompted for a task descrpition and an (optional) due date which is parsed to determine if it is a valid
+         * date format. A new Item is created using this information and then returned to the main function
+         * @returns newTask, an Item class containing a description and valid dd/mm/yyyy datetime
+         */
         static Item newItem()
         {
             Item task = new Item(); // the new task description and due date which will be returned
@@ -80,12 +100,13 @@ namespace ToDoList
             return task;
         }
 
-        /* function used to parse user provided console input to parse a yes or no string input 
+        /* 
+         * function used to parse user provided console input to parse a yes or no string input 
          * The function will convert the input to lowercase and truncate it to a single letter (so yes, no, y, n etc, answers are valid)
          * and return an appropriate true or false value. If an invalid string is provided then the user will be prompted to 
          * provide a valid response repeatedly until one is given
-         * @ input: a string provided by the user
-         * returns exitApp: a boolean value indicating whether the user entered yes or no
+         * @ param input: a string provided by the user
+         * @ returns exitApp, a boolean value indicating whether the user entered yes or no
          */
         static bool YesNo(string input)
         {
@@ -114,7 +135,7 @@ namespace ToDoList
     } 
 }
 
-// class used to represent a to do list entry, storing the description and optional date for said task
+// class used to represent a to do list entry, storing the description and optional due date for said task
 public class Item
 {
     public string Description { get; set; }
